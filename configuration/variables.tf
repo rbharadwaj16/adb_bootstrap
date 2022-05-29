d7ba624a4c4d0d0ac2662ba6767e8d5cc2369d6f
raghavendra.bharadwaj@coles.com.au <raghavendra.bharadwaj@coles.com.au>
2022-05-29 14:18:00 +0530

    Adding main vars.tf

---
+++ configuration/variables.tf
@@ -0,0 +1,11 @@
+variable "owner" {
+    description = "Owner name of the resource"
+}
+
+variable "purpose" {
+    description = "Purpose of the resource"
+}
+
+variable "location" {
+    description = "Location in which resource needs to be spinned up"
+}
