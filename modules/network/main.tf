c4fc3eadd78c0776eb68490503be00a98a0aa3eb
raghavendra.bharadwaj@coles.com.au <raghavendra.bharadwaj@coles.com.au>
2022-05-30 00:30:50 +0530

    Adding network module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -8,5 +8,5 @@ resource "azurerm_virtual_network" "vnet" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   address_space       = [var.address_space]
-
+  
 }
