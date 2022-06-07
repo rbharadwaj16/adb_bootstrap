28f43ccba0430fba3a2bba4bcf3bb3461b9e7d62
rbharadwaj16 <raghavendra.bharadwaj@coles.com.au>
2022-06-07 23:11:59 +0530

    Testing for loop in ntwk O/P

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -2,7 +2,9 @@
 #     value = azurerm_virtual_network.vnet.id
 # }
 
-output "vnet_id" {
-    value = tomap({ for k, s in azurerm_virtual_network.vnet : k => s.id})
+output "virtual_network_id" {
+    value = tomap({
+        for k, s in azurerm_virtual_network.vnet : k => s.id
+    })
   
 }
