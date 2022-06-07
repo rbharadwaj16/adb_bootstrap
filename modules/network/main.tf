53886a99665a5d9f78877ddabc401955a6616557
rbharadwaj16 <raghavendra.bharadwaj@coles.com.au>
2022-06-07 22:01:45 +0530

    Adding nsg associations

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -26,3 +26,9 @@ resource "azurerm_network_security_group" "nsg" {
   location = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 }
+
+resource "azurerm_subnet_network_security_group_association" "nsg_association" {
+  subnet_id = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+  
+}
